64feff13b2c6c02b59db00ae861c8400f62861c9
Stanislav Chyburiak <stanislav.chyburiak@gmail.com>
2024-08-23 02:27:07 +0100

    Lesson 5 - Homework right way.

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -21,10 +21,10 @@
                         <div class="hidden md:block">
                             <div class="ml-10 flex items-baseline space-x-4">
                                 <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                                <x-nav-link href="/" :active="request()->is('/')" type="a">Home</x-nav-link>
-                                <x-nav-link href="/about" :active="request()->is('about')" type="a">About</x-nav-link>
-                                <x-nav-link href="/contact" :active="request()->is('contact')" type="a">Contact</x-nav-link>
-                                <x-nav-link href="/contact" :active="request()->is('contact')" type="button">Test Button</x-nav-link>
+                                <x-nav-link href="/" :active="request()->is('/')">Home</x-nav-link>
+                                <x-nav-link href="/about" :active="request()->is('about')">About</x-nav-link>
+                                <x-nav-link href="/contact" :active="request()->is('contact')">Contact</x-nav-link>
+                                <x-nav-link type="button">Test Button</x-nav-link>
                             </div>
                         </div>
                     </div>
