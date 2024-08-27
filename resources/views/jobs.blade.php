af471c3339b52b9211a6607486c2f035735ea82a
Stanislav Chyburiak <stanislav.chyburiak@gmail.com>
2024-08-27 23:54:58 +0100

    Lesson 13

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -3,11 +3,14 @@
         Job Listings
     </x-slot:heading>
 
-    <ul>
+    <div class="space-y-4">
         @foreach($jobs as $job)
-            <li>
-                <a class="text-blue-500 hover:underline" href="/jobs/{{ $job['id'] }}"><strong>{{ $job['title'] }}</strong>: Pays {{ $job['salary'] }} per year.</a>
-            </li>
+            <a class="block px-4 py-6 border border-gray-200 roud-lg" href="/jobs/{{ $job['id'] }}">
+                <div class="font-bold text-blue-500 text-sm">{{ $job->employer->name }}</div>
+                <div>
+                    <strong>{{ $job['title'] }}</strong>: Pays {{ $job['salary'] }} per year.
+                </div>
+            </a>
         @endforeach
-    </ul>
+    </div>
 </x-layout>
