e1e6cac590b00423870c9a4861b6ce59efe828b4
Stanislav Chyburiak <stanislav.chyburiak@gmail.com>
2024-08-29 01:37:25 +0100

    Lesson 23

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Job;
-use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
 
 class JobController extends Controller
 {
@@ -48,8 +50,6 @@ class JobController extends Controller
             'salary' => ['required'],
         ]);
 
-        // TODO: authorize (on hold)
-
         $job->update([
             'title' => request('title'),
             'salary' => request('salary'),
@@ -59,7 +59,6 @@ class JobController extends Controller
     }
 
     public function destroy(Job $job) {
-        // TODO: authorize (on hold)
         $job->delete();
 
         return redirect('/jobs');
--- routes/web.php
+++ routes/web.php
@@ -8,26 +8,21 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'home');
 
 // Jobs
-// TODO: This is how to group using controller.
-//Route::controller(JobController::class)->group(function () {
-//    Route::get('/jobs', 'index');
-//    Route::get('/jobs/create', 'create');
-//    Route::post('/jobs', 'store');
-//    Route::get('/jobs/{job}', 'show');
-//    Route::get('/jobs/{job}/edit', 'edit');
-//    Route::patch('/jobs/{job}', 'update');
-//    Route::delete('/jobs/{job}', 'destroy');
-//});
+Route::get('/jobs', [JobController::class, 'index']);
+Route::get('/jobs/create', [JobController::class, 'create']);
+Route::post('/jobs', [JobController::class, 'store'])->middleware('auth');
+Route::get('/jobs/{job}', [JobController::class, 'show']);
 
-// TODO: Simple way to register group with default methods.
-Route::resource('jobs', JobController::class);
+Route::get('/jobs/{job}/edit', [JobController::class, 'edit'])->middleware('auth')->can('edit', 'job');
+Route::patch('/jobs/{job}', [JobController::class, 'update'])->middleware('auth')->can('edit', 'job');
+Route::delete('/jobs/{job}', [JobController::class, 'destroy'])->middleware('auth')->can('edit', 'job');
 // Jobs.
 
 // Auth
 Route::get('/register', [RegisterUserController::class, 'create']);
 Route::post('/register', [RegisterUserController::class, 'store']);
 
-Route::get('/login', [SessionController::class, 'create']);
+Route::get('/login', [SessionController::class, 'create'])->name('login');
 Route::post('/login', [SessionController::class, 'store']);
 Route::post('/logout', [SessionController::class, 'destroy']);
 // Auth.
